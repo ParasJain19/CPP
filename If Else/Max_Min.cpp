70879fc58079c0e3842e181ffb97cd09b9a066d7
ParasJain19 <135434591+ParasJain19@users.noreply.github.com>
2023-06-03 18:18:31 +0530

    Create Max_Min.cpp

---
+++ If Else/Max_Min.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers from which you want to find max and min:";
+    cin>>a>>b;
+    if(a>b)
+   { cout<<"Max is"<<a<<endl;
+    cout<<"MIn is"<<b<<endl;
+  }  else
+    {cout<<"Max is"<<b<<endl;
+cout<<"MIn is"<<a<<endl;
+}}
